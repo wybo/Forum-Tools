cc7d7296b8f2af664d227376181acd35dad7d9a6
Wybo Wiersma <mail@wybowiersma.net>
2011-05-02 00:39:15 +0100

    Cleaned up shell scripts

--- production/aascrape.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-cd /var/home/wybo/Dropbox/projects/hnscraper/production
-
-./scraper.rb &
--- production/agscrape.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-cd /home/wybo/Dropbox/projects/hnscraper/production
-
-./scraper.rb grun &
